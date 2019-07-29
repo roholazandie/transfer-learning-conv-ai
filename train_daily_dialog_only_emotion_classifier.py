bf9d52ae50f752de10dfe274e4bfa74014678c19
rohola <hilbert.cantor@gmail.com>
2019-07-29 16:02:09 -0600

    fixed typo

--- train_daily_dialog_only_emotion_classifier.py
+++ train_daily_dialog_only_emotion_classifier.py
@@ -213,7 +213,7 @@ def train():
         model.eval()
         with torch.no_grad():
             batch = tuple(input_tensor.to(config.device) for input_tensor in batch)
-            input_ids, mc_token_ids, lm_labels, mc_labels, token_type_ids, token_emotion_ids = batch
+            input_ids, mc_token_ids, lm_labels, mc_labels, token_type_ids = batch
             #logger.info(tokenizer.decode(input_ids[0, -1, :].tolist()))
             model_outputs = model(input_ids, mc_token_ids, token_type_ids=token_type_ids)
             lm_logits, mc_logits = model_outputs[0], model_outputs[1]  # So we can also use GPT2 outputs
